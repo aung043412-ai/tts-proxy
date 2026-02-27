6a1a7e839be404c48889efb023b2008b5c5e25b4
aung043412-ai <aung043412@gmail.com>
2026-02-27 22:58:14 +0700

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.get("/tts", async (req, res) => {
     const response = await fetch(
       "https://translate.google.com/translate_tts?ie=UTF-8&q=" +
         encodeURIComponent(text) +
-        "&tl=my&client=tw-ob"
+        "&tl=my&client=tw-ob",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+        },
+      }
     );
 
     const buffer = await response.arrayBuffer();
